022315ade709bdb74f2ae60cee4072bee3b725a4
Itallo Costa <itallo.costa@sympla.com.br>
2020-01-09 15:11:57 -0300

    add parameter params in the request authentication

--- src/Services/Authentication.php
+++ src/Services/Authentication.php
@@ -1,30 +1,26 @@
 <?php
-
-
 namespace RDStation\Services;
-
 use RDStation\Configuration\Routes;
 use RDStation\Helpers\BuildUrl;
-
 class Authentication
 {
     /** @var string $clientId */
     private $clientId;
-
     /** @var string $redirectUrl */
     private $redirectUrl;
-
+    /** @var array $parameters */
+    private $params;
     /**
      * Authentication constructor.
      * @param string $clientId
      * @param string $redirectUrl
      */
-    public function __construct(string $clientId, string $redirectUrl)
+    public function __construct(string $clientId, string $redirectUrl, array $params = [])
     {
         $this->clientId = $clientId;
         $this->redirectUrl = $redirectUrl;
+        $this->params = $params;
     }
-
     /**
      * @return string
      * @throws \RDStation\Exception\InvalidRouteException
@@ -34,9 +30,9 @@ class Authentication
         $url = BuildUrl::getUrlByRoute(Routes::AUTHENTICATION);
         $parameters = [
             'client_id' => $this->clientId,
-            'redirect_url' => $this->redirectUrl
+            'redirect_url' => $this->redirectUrl,
+            'params' => $this->params,
         ];
-
         return sprintf("%s?%s", $url, http_build_query($parameters));
     }
 }
